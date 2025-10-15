692b8a6f4b654735e61afed663bc932e9c105e10
Rebecca F <rebecca.fsq011@protonmail.com>
2025-10-15 03:59:16 +0200

    :sparkles: Implement User class with attributes

---
+++ part2/hbnb/app/models/user.py
@@ -0,0 +1,29 @@
+from base_model import BaseModel
+
+
+class User(BaseModel):
+    """
+    User class that inherits from BaseModel.
+    """
+    def __init__(self, email, password, first_name, last_name, is_admin=False):
+        """
+        Initialize a User instance with email, password, first name, last name,
+        and admin status.
+
+        Args:
+            email (str): The user's email address.
+            password (str): The user's password.
+            first_name (str): The user's first name.
+            last_name (str): The user's last name.
+            is_admin (bool): Indicates if the user has admin privileges.
+                            Default to False.
+        """
+        super().__init__()
+        self.email = email
+        self.password = password
+        self.first_name = first_name
+        self.last_name = last_name
+        self.is_admin = is_admin
+
+
+
